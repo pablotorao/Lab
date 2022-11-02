faf1c5066ccdd1284506344b1a3b96eff8c4cb1f
pablotorao <110969434+pablotorao@users.noreply.github.com>
2022-11-02 10:35:09 -0500

    Add files via upload

--- front.py
+++ front.py
@@ -6,29 +6,51 @@ import numpy as np
 import altair as alt
 from scipy import signal
 fig, ax = plt.subplots()
-def graphdisc(r,y):
-	fig, ax = plt.subplots()
+def graph3(r,y):
+	fig, ax = plt.subplots() #graph para cont (los invocas en el boton)
 	ax.plot(r,y)
 	ax.legend()
 	the_plot3.pyplot(fig)
 def graph(r,y,r2,y2):
         fig, ax = plt.subplots()
-        ax.plot(r, y, label="Signal")
+        ax.plot(r, y, label="Signal") #graph para serie
         ax.plot(r2, y2, label="System")
         ax.legend()
         the_plot.pyplot(fig)
 def graph2(r,y):
 	fig, ax = plt.subplots()
-	ax.plot(r, y, label="Convolution")
+	ax.plot(r, y, label="Convolution") #graph para cont
 	ax.legend()
 	the_plot2.pyplot(fig)
+def graph2disc(r,y):
+	fig, ax = plt.subplots()
+	ax.scatter(r, y, label="Convolution") #graph para disc
+	ax.legend()
+	the_plot2.pyplot(fig)
+def graphdisc(r,y):
+	fig, ax = plt.subplots()
+	ax.scatter(r, y, label="Convolution") #graph para disc
+	ax.legend()
+	the_plot1.pyplot(fig)
+def graph3disc(r,y):
+	fig, ax = plt.subplots()
+	ax.scatter(r, y, label="Convolution") #graph para disc
+	ax.legend()
+	the_plot3.pyplot(fig)
+def graph1(r,y):
+	fig, ax = plt.subplots()
+	ax.plot(r, y, label="Convolution") #graph para cont
+	ax.legend()
+	the_plot1.pyplot(fig)
 def option_type():
 	option_type = st.selectbox(
 	'Type',
 	('En que lo puedo ayudar?','Serie', 'Transformada',))
 	return option_type
+ones='1.'
 one='1'
 two='2'
+three= '3'
 p1=0.04
 option_type=option_type()
 def graph(r,y,r2,y2):
@@ -42,7 +64,14 @@ def type(one):
         'Funcion'+one,
         ('Exponencial Ae^-bt', 'Senoidal', 'Triangular', 'Rectangular', 'Rampa1','Rampa2','Rampa3')) #lo que cambies aqui toca cambiarlo en los if de abajo
 	return option1
+def discorcont(one):
+	option2 = st.selectbox(
+        'Funcion'+one,
+        ('Discreta','Continua')) #lo que cambies aqui toca cambiarlo en los if de abajo
+	return option2
+
 def contrec(which):
+
 	st.header("Funcion"+': '+which)
 	li = st.slider(' Periodo'+' '+which,-10,10, -5)
 	a = st.slider('Amplitud'+' '+which,-20,20, 1)
@@ -62,7 +91,7 @@ with col1:
 			li,a = contrec(one)
 
 		if a=='Rampa3':
-			li,ls,a=contrec(one)
+			li,a=contrec(one)
 
 		if a=='Rampa1':
 			li,a=contrec(one)
@@ -71,7 +100,20 @@ with col1:
 		if a == 'Exponencial Ae^-bt':
 			li, a= contrec(one)
 	if option_type== 'Transformada':
-		contrec(one)
+		c = discorcont(one)
+		if c=='Discreta':
+			contrec(one)
+			contrec(two)
+			contrec(three)
+			st.header('------------------------------')
+			nm1 = st.slider('# de muestras', -20, 20, 1)
+			fm = st.slider(' Frecuencia de muestreo', -10, 10, -5)
+		else:
+			contrec(one)
+			contrec(two)
+			contrec(three)
+			st.header('------------------------------')
+			fm = st.slider(' Frecuencia de muestreo', -10, 10, -5)
 with col2:
 	if st.button('Graficar '): 
 		if option_type== 'Serie':
@@ -81,9 +123,14 @@ with col2:
 				pass
 		if option_type== 'Transformada':
 				try:
-					the_plot3=st.pyplot(fig)
-					the_plot=st.pyplot(fig)
-					the_plot2=st.pyplot(fig)
+					if c == 'Discreta':
+						the_plot3=st.pyplot(fig)
+						the_plot1=st.pyplot(fig)
+						the_plot2=st.pyplot(fig)
+					else:
+						the_plot3 = st.pyplot(fig)
+						the_plot1 = st.pyplot(fig)
+						the_plot2 = st.pyplot(fig)
 				except:
 
 					pass
